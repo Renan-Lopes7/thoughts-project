4a2b58faa2ce705006f19cb9b9c56fd406aa067f
Renan Lopes <renan.blomloi@gmail.com>
2026-05-06 16:47:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.use(express.json());
 app.use(
     session({
         name: 'session',
-        secret: "nosso_secret", //vai ajudar a proteger as sessoes do usu
-        resave: false, //caiu a sessão ele vai desconectar 
+        secret: "nosso_secret", 
+        resave: false, 
         saveUninitialized: false,
         store: new FileStore({
             logFn: function () { },
@@ -44,8 +44,8 @@ app.use(
         }),
         cookie: {
             secure: false,
-            maxAge: 360000, //tempo em que ele dura
-            expires: new Date(Date.now() + 360000), //automaticamente expira em um dia 
+            maxAge: 360000, 
+            expires: new Date(Date.now() + 360000), 
             httpOnly: true,
         }
     }),
